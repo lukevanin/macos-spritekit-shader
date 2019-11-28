e798825e21086e059e208bd2aa47cc41407a437d
Luke Van In <lukevanin@gmail.com>
2019-11-28 17:34:31 +0200

    Initial Commit

---
+++ MacShaderTest/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MacShaderTest
+//
+//  Created by Luke Van In on 2019/11/28.
+//  Copyright © 2019 eventcloud. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
